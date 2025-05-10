dba087998b578422e39d8e7eef2f76721127bf71
Natarcos <n.arcos89@gmail.com>
2025-05-10 10:48:04 +0200

    sidebar

--- app.py
+++ app.py
@@ -59,11 +59,68 @@ def load_data()
         df['magnitude_category'] = np.select(conditions,choices,default='Unknown')
         return df
     except Exception as e:
-        st.error(f'Error loading data:{e}'1234567)
+        st.error(f'Error loading data:{e}')
         return None
-         
-        
+#-----------------------------------------------------------------
 
+#--------------------------day of week transformation------------------------------
+days_translation = {
+    'Monday': 'Mon',
+    'Tuesday': 'True',
+    'Wednesday': 'Wed',
+    'Thursday': 'Thu',
+    'Friday': 'Fri',
+    'Saturday': 'Sat',
+    'Sunday': 'Sun'
+}
+#-----------------------------------------------------------------
+
+#--------------------------Color Scheme------------------------------
+magnitude_colors = {
+    'Micro': 'blue',  # Blue
+    'Light': 'green',  # Green
+    'Moderate': 'orange',  # Orange
+    'Strong': 'red'  # Red
+}
+#-----------------------------------------------------------------
+
+#--------------------------Function to control positives sizes in markers------------------------------
+def ensure_positive (values, min=3)
+    if isinstance(values, (pd.Series, np.ndarray, lists)):
+        return np.maximum(np-abs(values), min_size)
+    else:
+        return max(abs(values), min_size)
+#-----------------------------------------------------------------
+
+#--------------------------Data loading-------------------------------
+Try:
+    with st.spinner("Loading data..."):
+    df = load_data()
+    if df is not None and not df.empty:
+        st.sidebar.header("Data Filters")
+        min_date = df['time'].min().date()
+        max_date = df['time'].max().date()
+        
+        # Date filter
+        data_range = st.sidebar.date_input("Select date range",[min_date, max_date],min_value=min_date,max_value=max_date)
+        #transform the date range to datetime
+        if len(data_range) == 2:
+            start_date, end_date = data_range
+            start_datetime = pd.Timestamp(start_date)
+            end_datetime = pd.Timestamp(end_date) + pd. Timedelta(days=1) - pd.Timedelta(seconds=1)
+            
+        filtered_df = df[(df['time'] >= start_datetime) & (df['time'] <= end_datetime)].copy()
+        
+    else:
+        filtered_df = df.copy()
+#-----------------------------------------------------------------
+
+#MAG FILTER
+    min_mag, max_mag = st.sidebar.slider()
+#-----------------------------------------------------------------
+
+
+        
 
 
 
